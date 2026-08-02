bf7082eef4be7dff6c01965f0864af0c85966c86
ice <239249615+rawcookie-s@users.noreply.github.com>
2026-08-01 23:56:36 -0400

    nothing

--- lua/libre_shop/detours.lua
+++ lua/libre_shop/detours.lua
@@ -1,11 +1,11 @@
 // nice things
 
-LibreSC.OriginalSetCreditsFor = LibreSC.OriginalSetCreditsFor or LibreSC.SetCreditsFor
+LibreSCOriginalSetCreditsFor = LibreSCOriginalSetCreditsFor or LibreSC.SetCreditsFor
 
 function LibreSC:SetCreditsFor(steamid, credits)
     local before = self:GetCreditsFor(steamid)
 
-    self:OriginalSetCreditsFor(steamid, credits)
+    LibreSCOriginalSetCreditsFor(self, steamid, credits)
 
     hook.Run("LibreSC::OnBalanceChange", steamid, before, credits)
 end
